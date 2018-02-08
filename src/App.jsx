92c9112e747d23a6383be20fd9b07d4ed9bb2be4
Hannie Dong <hdong92@gmail.com>
2018-02-07 17:12:07 -0800

    Create App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-App.jsx
+import React, { Component } from 'react';
+import Header from './components/Header';
+
+export default class App extends Component{
+  render(){
+    return(
+      <Header />
+      )
+  }
+}
