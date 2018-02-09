0beec3274562dae8898438e415113cab8b039714
Hannie Dong <hdong92@gmail.com>
2018-02-09 12:08:06 -0800

    Add styling to center align items.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar';
 export default class Header extends Component{
   render(){
     return(
-      <div className = 'container'>
+      <div className = 'header-container'>
         <h1> Hackathon Finder </h1>
         <SearchBar />
       </div>
