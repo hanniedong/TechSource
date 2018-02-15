abb54caa015a736dbd8e78d2c19449d9a839be97
Hannie Dong <hdong92@gmail.com>
2018-02-15 14:34:52 -0800

    Rename files.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { withGoogleMap, GoogleMap } from 'react-google-maps'
- import { PlaceMarker } from './placeMarker'
+ import { PlaceMarker } from './PlaceMarker'
 
 const ViewMap = withGoogleMap(props => (
   <GoogleMap
