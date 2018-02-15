8727d094ed61e73e316ec5d673f36949c4c02287
Hannie Dong <hdong92@gmail.com>
2018-02-15 02:38:32 -0800

    Edit eventbrite image source.

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -37,7 +37,7 @@ export default class EventList extends Component{
   render(){
     return(
       <div className = 'container'>
-        <img className="eventbrite_img" src="https://www.wixeventviewer.com/images/powered_by_eventbrite.png" alt=""/>
+        <img className="eventbrite_img" src="http://www.wixeventviewer.com/images/powered_by_eventbrite.png" alt=""/>
         {this.renderEvents()}
       </div>
     )
